5214f715bc64d41919bdb03645512a5358724476
Francisca <francisca.orwa@student.moringaschool.com>
2023-01-18 01:30:45 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (<div>
+    
+    <h1>Movies Page</h1>
+     <div>
+      {
+      movies.map((movie,index)=>(
+        <div key={index}>
+          <h3>"Name:"{movie.title}</h3>
+          <h4>"Time:" {movie.time}</h4>
+        <ul>
+          Genres
+         <li>{movie.genres}</li> 
+        </ul>
+        </div>
+      ))}
+     </div>
+
+    </div>
+    )
+};
+
 
 export default Movies;
